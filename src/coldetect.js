1acc6631bc76adfb8efddb42e2dce6990a41889a
Voraton <voraton.l@ku.th>
2015-03-23 18:12:39 +0700

    Sparkmoving and CheckCollision

---
+++ src/coldetect.js
@@ -0,0 +1,34 @@
+var checkPlayerSparkCollision = function( player , spark ) {
+    positionPlayer = player.getPosition();
+    positionSpark = spark.getPosition();
+    if (positionPlayer.x>positionSpark.x-60 && positionPlayer.x<positionSpark.x-20){
+        if (positionPlayer.y>positionSpark.y-30 && positionPlayer.y < positionSpark.y +30){
+            return 1;
+        }
+        else if (positionPlayer.y>positionSpark.y+30 && positionPlayer.y < positionSpark.y +90){
+            return 2;
+        }
+        else if (positionPlayer.y<positionSpark.y-30 && positionPlayer.y > positionSpark.y -90){
+            return 3;
+        }
+    }
+    else if (positionPlayer.x<positionSpark.x+60 && positionPlayer.x>positionSpark.x+20){
+        if (positionPlayer.y>positionSpark.y-30 && positionPlayer.y < positionSpark.y +30){
+            return 4;
+        }
+        else if (positionPlayer.y>positionSpark.y+30 && positionPlayer.y < positionSpark.y +90){
+            return 5;
+        }
+        else if (positionPlayer.y<positionSpark.y-30 && positionPlayer.y > positionSpark.y -90){
+            return 6;
+        }
+    }
+    else if (positionPlayer.x<=positionSpark.x+20 && positionPlayer.x>=positionSpark.x-20) {
+        if (positionPlayer.y<positionSpark.y+30 && positionPlayer.y >= positionSpark.y){
+            return 7;
+        }
+        else if (positionPlayer.y > positionSpark.y-30 && positionPlayer.y < positionSpark.y){
+            return 8;
+        }
+    }
+};
